d78b91de41a97d9904273294adb9d6a11f311a47
owner mc <solnysko@MacBook-Pro-Solnysko.local>
2020-11-26 19:45:35 +0300

    def function(s)

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -2,19 +2,17 @@ import sys
 # get in cycle argv[i] process each str
 if len(sys.argv) < 2:
 	exit()
+
+def reversecase(s):
+	return s[::-1].swapcase()
+
 n = len(sys.argv) - 1
 # s = sys.argv[n]#input()
-# i = len(s) - 1
-while n > 0: #- 1:
-	s = sys.argv[n]
-	i = len(s) - 1
-	while i >= 0:
-		if s[i].islower() == 1:#int(s[i]) >= 65 and int(s[i]) <= 90:
-			print(s[i].upper(), end='')#(int(s[i]) + 32)
-		else:
-			print(s[i].lower(), end='')
-		i -= 1
+# output = 
+while n > 1:
+	s = s[::-1]
+	# print(s, end="--\n")
 	if n != 1:
-		print(end=' ')
+		print(s, end=' ')
 	n -= 1
 print()
