5a6e69b6fea7062420a7d46a820f469c3f278c72
Dancy Dratini <ddratini@mi-d3.msk.21-school.ru>
2020-11-27 21:02:00 +0300

    00-02 regular expression match search

--- ex02/whois.py
+++ ex02/whois.py
@@ -8,11 +8,12 @@ if len(sys.argv) > 2:
     print('ERROR')
     exit()
 else:
+    result = re.match("[-+]?\d+$", sys.argv[1])
     #if sys.argv[1].isdigit() == 0:
     # if sys.argv[1].isnumeric() == 0: # fails
-    result = re.match("[-+]?\d+$", sys.argv[1])
     if result == 0:
         print('ERROR')
+    #elif sys.argv
     elif int(sys.argv[1]) == 0:
         print("I'm Zero")
     elif int(sys.argv[1]) % 2 == 0:
