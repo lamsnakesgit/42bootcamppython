daede74237d62f7ed8d17cf1fc4ec9e0e28b082e
Dancy Dratini <ddratini@mi-d5.msk.21-school.ru>
2020-11-30 21:22:07 +0300

    00-03 docstr, count cases,gens,input

---
+++ day00/ex03/count.py
@@ -0,0 +1,19 @@
+from string import punctuation
+
+def text_analyzer(s=None):
+    """ This function counts the number of upper characters, lower characters,
+    punctuation and spaces in a given text."""
+    if s == None: #0:
+        s = input('What is the text to analyse?\n')
+    lower = sum(i.islower() for i in s)
+    upper = sum(i.isupper() for i in s)
+    space = sum(i.isspace() for i in s)
+    puncts = sum(i in punctuation for i in s) #(
+    print(f"Text contains {len(s)} characters:\n- {upper} upper letters")
+    print("- " + str(lower) + " lower letters")
+    print("- " +  str(puncts) + " punctuation marks")
+    print("- " + str(space) + " spaces")
+    print(s)
+
+#text_analyzer(input())
+
