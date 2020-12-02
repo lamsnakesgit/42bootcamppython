5b6ac35d820c6f8cdb1274e4d91af78e8ef24c45
Dancy Dratini <ddratini@mi-d3.msk.21-school.ru>
2020-12-02 19:50:30 +0300

    0-4 error as not int or not 2 args

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -1,13 +1,18 @@
+import sys
 import string
 
 def operations():
-    nums = [int(num) for num in input().split()]
-    if len(nums) == 2:
-        print(f'{"+Sum:":<15}' + f'{nums[0]+nums[1]}')
-        print(f'{"-Difference:":<15}' + f'{nums[0]-nums[1]}')
-        print(f'{"*Product:":<15}' + f'{nums[0]*nums[1]}')
-        print(f'{"*Quotient:":<15}' + f'{nums[0]/nums[1]}')
-        print(f'{"%Remainder:":<15}' + f'{nums[0]%nums[1]}')
+    if len(sys.argv) < 3:
+        print("InputError: two arguments needed")
+    elif len(sys.argv) == 3:
+        try:
+            print(f'{"Sum:":<15}' + f'{int(sys.argv[1])+int(sys.argv[2])}')
+            print(f'{"Difference:":<15}' + f'{int(sys.argv[1])-int(sys.argv[2])}')
+            print(f'{"Product:":<15}' + f'{int(sys.argv[2])*int(sys.argv[1])}')
+            print(f'{"Quotient:":<15}' + f'{int(sys.argv[1])/int(sys.argv[2])}')
+            print(f'{"Remainder:":<15}' + f'{int(sys.argv[1])%int(sys.argv[2])}')
+        except ValueError:
+            print("InputError: only numbers")
     else:
         print("InputError:too many argumnets")
 operations()
