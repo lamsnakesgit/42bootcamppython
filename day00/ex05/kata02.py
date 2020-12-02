870b21e7156a28eb4e8742d3aa46cf6753be9a6c
Dancy Dratini <ddratini@mi-d3.msk.21-school.ru>
2020-12-02 22:36:29 +0300

    - 42 \

---
+++ day00/ex05/kata02.py
@@ -0,0 +1,10 @@
+import datetime
+
+t = (3,30,2019,9,25)
+
+# print(()) # ()
+# for i in 
+d = datetime.datetime(t[2], t[3], t[4], t[0], t[1])
+print(d)
+    # print(i, t[i])
+print(d.strftime("%m/%d/%Y %H:%M"))
---
+++ day00/ex05/kata03.py
@@ -0,0 +1,3 @@
+phrase = "The right format"
+
+print("%42s" % phrase.rjust(42,'-'), end='')
