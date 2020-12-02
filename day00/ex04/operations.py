d8cddc5c785d8ed92d5205f909711f268ee27e14
Dancy Dratini <ddratini@mi-d3.msk.21-school.ru>
2020-12-02 19:32:25 +0300

    same format <

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -3,15 +3,14 @@ import string
 def operations():
     nums = [int(num) for num in input().split()]
     if len(nums) == 2:
-        print('Sum:' '%9s' % f"{nums[0]+nums[1]}")
-        print('Difference:' '%2s' % f"{nums[0]-nums[1]}")
-        print('Product:' '%8s' % f"{nums[0]*nums[1]}")
-        print('Product:'.ljust(12 + 2) + f'{nums[0]*nums[1]}')
-        #print('Quotient:ii' '{:<15}'.format("quot:")  f'{nums[0]+nums[1]}')
-        print(f'{"Quotient:":<15}' + f'{nums[0]/nums[1]}')
-        print('Remainder:'.ljust(len("difference") + 2) + f'{nums[0]%nums[1]}')
-        print("randstr".ljust(4,'p'))
-        print(len("difference") + 2)
+        #print('Sum:' '%9s' % f"{nums[0]+nums[1]}")
+        print(f'{"+Sum:":<15}' + f'{nums[0]+nums[1]}')
+        print(f'{"-Difference:":<15}' + f'{nums[0]-nums[1]}')
+        #print('Difference:' '%9s' % f"{nums[0]-nums[1]}")
+        print(f'{"*Product:":<15}' + f'{nums[0]*nums[1]}')
+        #print('Quotient:' '%9s' % f"{nums[0]/nums[1]}")
+        print(f'{"*Quotient:":<15}' + f'{nums[0]/nums[1]}')
+        print(f'{"%Remainder:":<15}' + f'{nums[0]%nums[1]}')
     else:
         print("InputError:too many argumnets")
 operations()
